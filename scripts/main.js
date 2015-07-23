4b1f00d57b5310f31f0cb3bebcb51a6726c36b04
Timotius Nugroho Chandra <timotius.nc@gmail.com>
2015-07-23 17:07:57 +0700

    1. Apply material design layout 2. Add touch icons 3. Prune unused jekyll css

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+(function () {
+  'use strict';
+  
+  $(document).ready(function() {
+    $('.button-collapse').sideNav({
+      menuWidth: 256
+    });
+    
+    $('.parallax').parallax();    
+  });
+})();
